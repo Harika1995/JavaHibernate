4482d522b55628e6c081da3884a2c7e8808fa9b5
harika1995 <nagaharika.n@gmail.com>
2019-12-09 23:12:32 -0600

    Update with dynamic query in hql

--- HibernateHqlExample/src/main/java/com/harika/HibernateHqlExample/HibernateQueryLanguage.java
+++ HibernateHqlExample/src/main/java/com/harika/HibernateHqlExample/HibernateQueryLanguage.java
@@ -26,7 +26,21 @@ public class HibernateQueryLanguage {
         List<Object []> result =query.list();
         Object[] obj = result.get(0);
         System.out.println(obj[0]+"\t"+obj[1]+"\t"+obj[2]+"\t"+obj[3]);
-        
+        System.out.println("-----------------------");
+        System.out.println("Dynamic query");
+        Query query1 = ses.getNamedQuery("my_query");
+        query1.setParameter("mail", "%gmail.com");
+        List<Employee> dynamic_result = query1.list();
+        for(Employee emp : dynamic_result) {
+        	System.out.println(emp.getEno()+"\t"+emp.getFirstName()+"\t"+emp.getLastName()+"\t"+emp.getEmail());
+        }
+        System.out.println("Emloyee details with the employee no lessthan or equal to 103");
+        Query query2 = ses.getNamedQuery("my_query1");
+        query2.setParameter("num", 102);
+        List<Employee> dynamic_result1 = query2.list();
+        for(Employee emp1 : dynamic_result1) {
+        	System.out.println(emp1.getEno()+"\t"+emp1.getFirstName()+"\t"+emp1.getLastName()+"\t"+emp1.getEmail());
+        }
 	}
 
 }
